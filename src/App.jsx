25722bd8ac6450fd09ddd16d32883da7ff104f8b
Holytech <akeemqudus2016@gmail.com>
2022-11-02 00:57:58 +0100

    First commit: project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Project Setup</h1>
+    </div>
+  );
+}
+
+export default App;
